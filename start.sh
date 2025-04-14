a6385b9c928628640e455033edba24d37fa01bd9
coffeepea <tastymilkys@proton.me>
2025-04-14 21:14:23 +0700

    add multiple start mode for sway

--- start.sh
+++ start.sh
@@ -11,4 +11,10 @@ export XDG_SESSION_DESKTOP=sway
 export QT_SCREEN_SCALE_FACTORS="1;1"
 export MOZ_ENABLE_WAYLAND=1
 
-exec sway
+if [ "$1" == "ext" ]; then
+    exec sway --unsupported-gpu -c ~/.config/sway/extern
+elif [ "$1" == "multi" ]; then
+    exec sway --unsupported-gpu -c ~/.config/sway/multi
+else
+    exec sway --unsupported-gpu
+fi
